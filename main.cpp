6584bade91a6fab664bf2cf13a00c9b5b1d858b7
Ray <b08901113@ntu.edu.tw>
2023-04-21 23:28:16 +0800

    final

--- main.cpp
+++ main.cpp
@@ -9,15 +9,12 @@
 // You can use LED1/LED2/LED3/LED4 if any is connected to PWM capable pin,
 // or use any PWM capable pin, and see generated signal on logical analyzer.
 PwmOut led(PWM_OUT);
-PwmOut led2(D6);
 
 int main()
 {
     // specify period first
     led.period(0.05f);      // 0.05 second period
     led.write(0.50f);      // 50% duty cycle, relative to period
-    led2.period(0.05f);
-    led2.write(0.50f);
     //led = 0.5f;          // shorthand for led.write()
     //led.pulsewidth(2);   // alternative to led.write, set duty cycle time in seconds
     while (1);
